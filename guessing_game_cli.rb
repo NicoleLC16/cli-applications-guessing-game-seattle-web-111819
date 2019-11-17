388189988005421fa6fff0884675dfe5de3a745a
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-17 18:20:26 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,9 +5,10 @@ def run_guessing_game
   puts "Guess a number between 1 and 6"
   input = gets.chomp
   
-  if 
+  if user == 'exit'
+    puts "Goodbye!"
     
-    input == computer_number
+  elsif input == computer_number
     puts "You guessed the correct number!"
     
   else
